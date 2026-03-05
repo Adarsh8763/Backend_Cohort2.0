149087ebc126b0f63b190f05ece6074c4148a08e
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-03-06 05:18:55 +0530

    added backend_112 (face expression detection feature)

---
+++ moodify (backend_112)/Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FaceExpression from './features/expression/pages/FaceExpression'
+import "./features/shared/global.scss"
+
+const App = () => {
+  return (
+    <FaceExpression/>
+  )
+}
+
+export default App
