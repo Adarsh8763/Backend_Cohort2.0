32ac748bee90251a0f087475ab1adb298293fa5a
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-03-08 17:54:45 +0530

    Deployment ready

--- backend_101_insta-clone/Frontend/src/features/post/services/post.api.jsx
+++ backend_101_insta-clone/Frontend/src/features/post/services/post.api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/post",
+    baseURL: "https://backend-cohort2-0-1-e72l.onrender.com/",
     withCredentials: true
 })
 
