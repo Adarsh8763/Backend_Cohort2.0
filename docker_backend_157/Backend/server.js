d6c3bb5da24cc03cc16ceadce8cb0e23412ec037
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-05-29 09:45:49 +0530

    feat(docker): restructure Dockerfile and add docker-compose

--- docker_backend_157/Backend/server.js
+++ docker_backend_157/Backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express()
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "Hello, World!" })
+    return res.status(200).json({ message: "Hello, World! Mera naam Adarsh" })
     // console.log("Root endpoint was accessed")
 })
 
