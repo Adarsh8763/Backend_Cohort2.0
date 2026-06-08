4ee2258f9441665fc4c51f3f6c052dc4cf05ae02
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-06-08 16:48:55 +0530

    feat: initialize project with Vite and React setup
    - Added main entry point (main.jsx) for React application
    - Created Vite configuration file (vite.config.js) for development server
    - Included global styles in index.css
    - Added SVG assets for React and Vite logos

---
+++ Capstone_Project/sandbox/server/kubernetes/service.js
@@ -0,0 +1,36 @@
+import { k8sCoreV1Api } from "./config.js"
+
+export async function createService(sandboxId) {
+    const serviceManifest = {
+        apiVersion: "v1",
+        kind: "Service",
+        metadata: {
+            name: `sandbox-service-${sandboxId}`,
+            labels: {
+                app: "sanbox",
+                sandboxId: sandboxId
+            }
+        },
+        spec: {
+            selector: {
+                app: "sanbox",
+                sandboxId: sandboxId
+            },
+            type: "ClusterIP",
+            ports: [
+                {
+                    port: 80,
+                    targetPort: 5173,
+                    name: "http"
+                }
+            ]
+        }
+    }
+
+    const response = await k8sCoreV1Api.createNamespacedService({
+        namespace: "default",
+        body: serviceManifest
+    });
+
+    return response
+}
--- Capstone_Project/sandbox/server/src/app.js
+++ Capstone_Project/sandbox/server/src/app.js
@@ -1,5 +1,8 @@
 import express from "express"
 import morgan from "morgan"
+import { v7 as uuid } from "uuid"
+import { createService } from "../kubernetes/service.js"
+import { createPod } from "../kubernetes/pod.js" 
 
 const app = express()
 
@@ -13,5 +16,28 @@ app.get("/api/sandbox/health", (req, res) => {
         "status": "ok"
     })
 })
+
+app.post("/api/sandbox/start", async (req, res) => {
+    console.log("START API HIT");
+
+    try {
+        const sandboxId = uuid();
+
+        await Promise.all([
+            createPod(sandboxId),
+            createService(sandboxId)
+        ]);
+
+        return res.status(201).json({
+            message: "Sandbox environment is being created",
+            sandboxId
+        });
+    } catch (err) {
+        console.error("ERROR:", err);
+        return res.status(500).json({
+            error: err.message
+        });
+    }
+});
 
 export default app
