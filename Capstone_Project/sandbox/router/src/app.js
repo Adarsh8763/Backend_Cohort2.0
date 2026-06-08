f8f332decf35c4c606cb32e03a24566ed624bd2e
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-06-08 18:17:20 +0530

    feat(router): initialize router service with Express and proxy middleware
    - Created package.json for router service with dependencies and scripts.
    - Implemented server.js to start the Express server on port 3000.
    - Developed app.js to handle health check and readiness endpoints.
    - Added dynamic proxy middleware to forward requests based on sandbox ID.

---
+++ Capstone_Project/sandbox/router/src/app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import morgan from 'morgan'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+
+
+const app = express()
+
+app.use(morgan('dev'))
+
+app.get("/api/status/healthz", (req, res) => {
+    res.status(200).json({ status: "ok" })
+})
+
+app.get("/api/status/readyz", (req, res) => {
+    res.status(200).json({ status: "ready" })
+})
+
+const proxies = {}
+
+function getProxy(sandboxId){
+    const target = `http://sandbox-service-${sandboxId}` // Jaha request forward karna hai
+
+    if(!proxies[sandboxId]){
+        proxies[sandboxId] = createProxyMiddleware({
+            target,
+            changeOrigin: true,
+            ws: true,
+        });
+    }
+    return proxies[sandboxId];
+}
+
+app.use((req, res, next) => {
+    const host = req.headers.host
+    const sandboxId = host.split('.')[0] // Extract sandbox ID from subdomain
+
+    return getProxy(sandboxId)(req, res, next) // "Sandbox ID ke hisab se proxy middleware lao, aur us middleware ko current request (req, res, next) ke saath execute kar do."
+})
+
+export default app
