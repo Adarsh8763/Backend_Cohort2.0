82b1a406057e98fcb9306fe4a77452c2dfd6300e
Adarsh8763 <adarshkumarkamal8763@gmail.com>
2026-07-05 18:05:12 +0530

    Add initial HTML structure, CSS styles, and Vite logo
    - Created index.html with basic HTML structure and linked assets.
    - Added index-ZUgWRkGb.css for styling the application.
    - Included Vite logo (vite.svg) for branding in the application.

--- perplexity_backend_120/Backend/src/app.js
+++ perplexity_backend_120/Backend/src/app.js
@@ -2,11 +2,18 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import morgan from "morgan"
 import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
+app.use(express.static("./public"))
+
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -19,5 +26,10 @@ import chatRouter from "./routes/chat.routes.js"
 //Use Routes
 app.use("/api/auth", authRouter)
 app.use("/api/chats", chatRouter)
+
+app.use('*name', (req,res)=>{
+    // res.send("This is wild card")
+    res.sendFile(path.join(__dirname, "..", "/public/index.html"))
+})
 
 export default app
